a880d28871c55a73d5c9c7471751df8ef986ae2e
AndreyCoder <andreyme05@gmail.com>
2022-06-04 00:27:09 -0600

    Boton

--- client/src/components/landing-page/LandingPage.jsx
+++ client/src/components/landing-page/LandingPage.jsx
@@ -5,7 +5,7 @@ function LandingPage() {
   return (
     <div className='container'>
       <div>
-        <h1 className='titulo'>Bienbenidos a Poke Page</h1>
+        <h1 className='titulo'>Bienvenidos a Poke Page</h1>
       </div>
       <div>
         <button className='boton'>Ir a inicio</button>
