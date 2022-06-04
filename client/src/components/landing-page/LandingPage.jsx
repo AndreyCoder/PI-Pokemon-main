fd4ef5cd5dc20f2b3f605370530f1ee01e51399d
AndreyCoder <andreyme05@gmail.com>
2022-06-04 11:22:32 -0600

    Landing page

--- client/src/components/landing-page/LandingPage.jsx
+++ client/src/components/landing-page/LandingPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './landing-page.css'
+import image from '../../img/cover-lp.jpg'
 
 function LandingPage() {
   return (
     <div className='container'>
-      <div>
-        <h1 className='titulo'>Bienvenidos a Poke Page</h1>
-      </div>
-      <div>
+      <img
+        src={image}
+        alt=''
+        className='img-landing'
+      />
+      <h1 className='titulo'>Bienvenidos a Poke Page</h1>
+      <div className='btn'>
         <button className='boton'>Ir a inicio</button>
       </div>
     </div>
