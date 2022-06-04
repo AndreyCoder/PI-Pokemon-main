e71487c2538750a88ac1da96eb4adf9cf7d27b96
AndreyCoder <andreyme05@gmail.com>
2022-06-04 00:04:45 -0600

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LandingPage from './components/landing-page/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/components/landing-page/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './landing-page.css'
+
+function LandingPage() {
+  return (
+    <div className='container'>
+      <div>
+        <h1 className='titulo'>Bienbenidos a Poke Page</h1>
+      </div>
+      <div>
+        <button className='boton'>Ir a inicio</button>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
