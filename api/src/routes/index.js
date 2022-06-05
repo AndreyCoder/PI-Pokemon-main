a0e528d8e3e56f50872c9fe663da0d9a8ef9b42e
AndreyCoder <andreyme05@gmail.com>
2022-06-05 10:35:33 -0600

    Database

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+
 const router = Router();
 
 // Configurar los routers
